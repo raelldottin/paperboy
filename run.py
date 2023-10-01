c1baa8714e2ed9c906abdf20c1106f7e17419c6a
Raell Dottin <raell.dottin@gmail.com>
2023-10-01 08:59:04 -0400

    change to avoid duplicate posts
    This modification includes a get_existing_post_titles function that fetches the titles of existing posts, and the main loop checks whether the new post's title is already in the set of existing titles before creating a new post.

--- run.py
+++ run.py
@@ -47,6 +47,16 @@ def get_blog_id(blogger_service):
     return None
 
 
+def get_existing_post_titles(blogger_service, blog_id):
+    try:
+        posts = blogger_service.posts().list(blogId=blog_id).execute()
+        existing_titles = set(post["title"] for post in posts.get("items", []))
+        return existing_titles
+    except Exception as e:
+        print(f"Error fetching existing post titles: {str(e)}")
+        return set()
+
+
 def rate_limited(requests_per_minute):
     interval = 60 / requests_per_minute
 
@@ -133,6 +143,9 @@ if __name__ == "__main__":
     json_file_path = args.json_file
     blog_posts = read_json_file_from_github(github_repo, json_file_path)
 
+    # Fetch existing post titles
+    existing_titles = get_existing_post_titles(blogger_service, blog_id)
+
     if blog_posts is not None and "posts" in blog_posts:
         for post in blog_posts["posts"]:
             title = post.get("title")
@@ -140,15 +153,21 @@ if __name__ == "__main__":
             post_date_str = post.get("post_date")
 
             if title and content and post_date_str:
-                post_date = datetime.strptime(post_date_str, "%Y-%m-%dT%H:%M:%S")
-                current_time = datetime.now()
+                # Check if the title already exists
+                if title in existing_titles:
+                    print(f'Skipping duplicate blog post with title "{title}"')
+                else:
+                    post_date = datetime.strptime(post_date_str, "%Y-%m-%dT%H:%M:%S")
+                    current_time = datetime.now()
 
-                if post_date > current_time:
-                    time_difference = (post_date - current_time).total_seconds()
-                    print(f"Waiting for {time_difference} seconds until {post_date}")
-                    time.sleep(time_difference)
+                    if post_date > current_time:
+                        time_difference = (post_date - current_time).total_seconds()
+                        print(
+                            f"Waiting for {time_difference} seconds until {post_date}"
+                        )
+                        time.sleep(time_difference)
 
-                create_blog_post(blogger_service, title, content, blog_id)
+                    create_blog_post(blogger_service, title, content, blog_id)
             else:
                 print(
                     f"Skipping post with missing title, content, or post_date: {post}"
